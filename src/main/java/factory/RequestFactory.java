307a83f9add69cd8905d1745bf263869f0222280
marlin-reso <sanju.reso@gmail.com>
2025-11-04 17:05:36 +0530

    test

--- src/main/java/factory/RequestFactory.java
+++ src/main/java/factory/RequestFactory.java
@@ -1,13 +1,29 @@
 package factory;
 
+import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+
+
 
 public class RequestFactory {
 	
-	public static RequestSpecification getRequestSpec() {
+	
+	public static void main(String[] args) {
 		
-		return given().
+		Response resp = given()
+				.contentType(ContentType.JSON)
+				.when()
+				.get("http://localhost:3000/products");
+		
+		
+		
+		System.out.println(resp.getStatusCode());
+		System.out.println(resp.asPrettyString());
 	}
+	
+	
+	
 	
 	
 
